2ff453fd23a6bf14308502fd20aea86cfd766284
dannyid <danielshekhtman@gmail.com>
2014-12-13 21:18:55 -0500

    adding bodyParser stuff

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 var appPort =  process.env.PORT || 3000;
 
 var express = require('express'), 
+    bodyParser =  require( 'body-parser' ),
     app = express(),
     http = require('http'),
     server = http.createServer(app)
+
+
+app.use( bodyParser.urlencoded( {extended: true} ) );
+app.use( bodyParser.json() );
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -12,8 +17,9 @@ app.set('view options', { layout: false });
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
+app.all("/", function(req, res){
   res.render('home.jade');
+  console.log(req.body);
 });
 
 app.get("/:url", function(req, res){
